56d66815d9edf2c14367185c8b1215c4f2998458
VladScv <ddcllrnsbrv@uoc.edu>
2023-07-10 23:01:40 +0200

    Initial commit

---
+++ src/components/escenas/fondoImagenes.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useCallback } from "react";
+interface FondoImagenesProps {
+  indiceImagen: number;
+  imagenes: string[];
+}
+
+const FondoImagenes = (props: FondoImagenesProps) => {
+  const { indiceImagen, imagenes } = props;
+
+  const actualizarFondo = useCallback(() => {
+
+    const imagenFondo = imagenes[indiceImagen % imagenes.length];
+    document.body.style.backgroundImage =`url(/${imagenFondo})`;
+    document.body.style.backgroundSize = "cover";
+    document.body.style.backgroundPosition = "center";
+    document.body.style.backgroundRepeat = "no-repeat";
+    document.documentElement.style.height = "100%";
+    document.body.style.height = "100%";
+  }, [indiceImagen, imagenes]);
+
+  useEffect(() => {
+    actualizarFondo();
+  }, [actualizarFondo]);
+
+  return null;
+};
+
+export default FondoImagenes;
+// 
